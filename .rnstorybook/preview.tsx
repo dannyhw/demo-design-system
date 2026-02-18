a94c434d3380018f19ea5a81cbfcba8e90f96cc8
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2026-02-18 11:48:23 +0000

    fix

--- .rnstorybook/preview.tsx
+++ .rnstorybook/preview.tsx
@@ -1,24 +1,18 @@
 import type { Preview } from "@storybook/react-native";
-import { Appearance } from "react-native";
 
 const preview: Preview = {
   parameters: {
     layout: "padded",
-    // storybookUIVisibility: "hidden",
     backgrounds: {
       options: {
-        // 👇 Default options
         dark: { name: "dark", value: "#000" },
         light: { name: "plain", value: "#fff" },
-        // 👇 Add your own
         app: { name: "app", value: "#eeeeee" },
       },
     },
   },
   initialGlobals: {
-    // 👇 Set the initial background color
     backgrounds: {
-      // value: Appearance.getColorScheme() === "dark" ? "dark" : "plain",
       value: "dark",
     },
   },
